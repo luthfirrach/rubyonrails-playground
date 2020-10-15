0b58e514ce32877c0ef2588399eabba435eff084
luthfirrach <luthfir.rachman@gmail.com>
2020-10-16 06:38:24 +0700

    Improve - categories/show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,10 @@
 class CategoriesController < ApplicationController
   def index
+    render plain: params[:id].inspect
   end
   def show
     # render plain: params[:id].inspect
+    @cats = MCategory.all
     @ads = Ad.where(m_category_id: params[:id])
   end
 end
