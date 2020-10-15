96571455c932e2f3529c984934af7377d0d097f8
luthfirrach <luthfir.rachman@gmail.com>
2020-10-16 06:16:01 +0700

    Add - categories/show - vc

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,8 @@
 class CategoriesController < ApplicationController
   def index
-    
+  end
+  def show
+    # render plain: params[:id].inspect
+    @ads = Ad.where(m_category_id: params[:id])
   end
 end
