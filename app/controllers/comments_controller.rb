07f39deecf28e023b6ec096d5a56e2f7e076308c
luthfirrach <luthfir.rachman@gmail.com>
2020-10-16 04:50:24 +0700

    Add - comment features

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    @ad = Ad.find(params[:ad_id])
+    @comment = @ad.comments.create(comment_params)
+    redirect_to ad_path(@ad)
+  end
+
+  private def comment_params
+    params.require(:comment).permit(:username, :body)
+  end
 end
